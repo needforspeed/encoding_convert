f25a549454004d10de24097ea5821d34cdcff938
Yuncong Zhang <njitzyc@gmail.com>
2013-03-15 20:16:11 +0000

    1. check if input_folder exists. 2. make output dir.

--- encoding_convert.sh
+++ encoding_convert.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+if [ ! -d input_folder ]
+then
+  echo "No input_folder"
+  exit 1
+fi
+
+if [ ! -d output_folder ]
+then
+  mkdir output_folder
+fi
+
 for file in input_folder/*
 do
-	filename=`basename "$file"`;
-	echo "$filename";
-	iconv -c --verbose -f GBK -t UTF-8 "$file" -o "output_folder/$filename";
+  filename=`basename "$file"`;
+  echo "Converting $filename";
+  iconv -c --verbose -f GBK -t UTF-8 "$file" -o "output_folder/$filename";
 done;
+
